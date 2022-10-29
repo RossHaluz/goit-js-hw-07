9d0b6a3b6ab69f03385ea3fed63e77d2ce74f3c7
Rostyslav <rossgaluzinskiy@gmail.com>
2022-10-29 15:25:55 +0300

    fix bag

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,15 +6,17 @@ console.log(galleryItems);
 const galleryPhotos = document.querySelector('.gallery');
 
 let items = galleryItems.map(({ preview, original, description }) => 
-`<a class="gallery__item" href="${original}">
+`<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
-</a>`
+</a></li>`
 ).join('')
 
 
 galleryPhotos.innerHTML = items
 
 const lightbox = new SimpleLightbox('.gallery a', {  
+  captions: true,
+  captionPosition: 'bottom',
     captionsData: "alt",
     captionDelay: 250,
 });
